9aaaa6a305169636936f0df5166b0b3e747eb8d4
Nabil <nabilehsanul@gmail.com>
2026-06-11 02:07:57 +0600

    init

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import MainLayout from '../layouts/MainLayout';
+
+const AppRoutes = () => {
+    return (
+        <div>
+          <Routes>
+             {/* <Route index element={<Home />} />
+             <Route path="about" element={<About />} /> */}
+             
+             <Route element={<MainLayout/>}>
+                <Route path="/"element ={<Home/>}/>
+                <Route path="about"element ={<About/>}/>
+             </Route>
+
+          </Routes>
+        </div>
+    );
+};
+
+export default AppRoutes;
