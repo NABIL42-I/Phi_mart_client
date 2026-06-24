54d7598899e15f509aeaf87d69267d0472968c97
Nabil <nabilehsanul@gmail.com>
2026-06-25 02:48:12 +0600

    Login Required add on productDetails

--- src/components/ProductDetails/AddToCartButton.jsx
+++ src/components/ProductDetails/AddToCartButton.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { FaCheck, FaShoppingCart } from "react-icons/fa";
 import { FaMinus, FaPlus } from "react-icons/fa6";
 import useCartContext from "../../hooks/useCartContext";
+import useAuthContext from "../../hooks/useAuthContext";
+import { Link } from "react-router";
 
 const AddToCartButton = ({ product }) => {
 
-    
+  const {user} = useAuthContext
   const [quantity, setQuantity] = useState(1);
   const [isAdding, setIsAdding] = useState(false);
   const [isAdded, setIsAdded] = useState(false);
@@ -84,10 +86,29 @@ const AddToCartButton = ({ product }) => {
           <FaPlus className="h-4 w-4" />
         </button>
       </div>
-      <button
+
+<div class="fixed bottom-5 right-5 z-50 animate-fade-in">
+  {!user ? (
+    <div class="flex items-center gap-3 backdrop-blur-md bg-white/70 dark:bg-zinc-900/70 border border-zinc-200 dark:border-zinc-800 px-4 py-3 rounded-xl shadow-lg shadow-zinc-200/50 dark:shadow-none">
+      <div class="flex h-8 w-8 items-center justify-center rounded-lg bg-indigo-50 dark:bg-indigo-950/50 text-indigo-600 dark:text-indigo-400">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+        </svg>
+      </div>
+      
+      <div class="text-sm font-medium text-zinc-700 dark:text-zinc-300">
+        <span>Login required to add items to cart</span>
+        <Link to="/login" class="ml-3 text-indigo-600 dark:text-indigo-400 hover:text-indigo-500 font-semibold transition-colors underline underline-offset-4">
+          Sign In
+        </Link >
+      </div>
+    </div>
+  ) : ""}
+</div>
+       <button
         className="btn btn-primary w-full"
         onClick={addToCart}
-        disabled={isAdding || isAdded || product.stock === 0}
+        disabled={ !user ||isAdding || isAdded || product.stock === 0}
       >
         {isAdding ? (
           <span className="flex items-center">
